c6d45433ef03048f82430bacfead68b3b7ff21a3
CinQue Wilson <parallax707@gmail.com>
2022-02-28 17:06:25 -0500

    Front end

--- survey/src/app/questions/questions.component.ts
+++ survey/src/app/questions/questions.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionsComponent implements OnInit {
 
-  constructor() { }
+  onSubmit(data){
+    this.http.post('http://localhost:9092/api/tickets/', data).subscribe((result)=>{
+      console.warn("result", result)
+    })
+    console.warn(data);
+  }
+
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
   }
